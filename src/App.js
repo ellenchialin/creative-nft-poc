a04d845f30349a8482d3b4671267989de137cc58
ellenchen <chialin76@gmail.com>
2022-08-02 01:25:20 +0800

    Display location time from search query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+
+import './app.css'
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date())
+  const [timezone, setTimezone] = useState('')
+  const [location, setLocation] = useState('')
+
+  const getLocalTime = async () => {
+    const locationQuery = new URLSearchParams(window.location.search).get(
+      'location'
+    )
+
+    console.log('location: ', locationQuery)
+
+    const res = await fetch(
+      `https://timezone.abstractapi.com/v1/current_time/?api_key=fe70b53cf8d54ed4ac6b92eff7547006&location=${locationQuery}`
+    )
+    const data = await res.json()
+
+    setTimezone(data.timezone_location)
+    setLocation(data.timezone_location)
+
+    console.log('Timezone: ', data.timezone_location)
+    console.log('Datetime: ', data.datetime)
+  }
+
+  useEffect(() => {
+    getLocalTime()
+  }, [])
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => setCurrentTime(new Date()), 1000)
+
+    return () => {
+      clearInterval(timeInterval)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {timezone && (
+        <div className='time-container'>
+          <h2>{location}</h2>
+          <p className='time'>
+            {currentTime.toLocaleString('en-US', {
+              timeZone: timezone
+            })}
+          </p>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
