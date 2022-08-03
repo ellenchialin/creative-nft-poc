560ae10e2e6be61d7508268f343e91dea266a8fa
ellenchen <chialin76@gmail.com>
2022-08-03 22:51:21 +0800

    Remove wallet related function and mint

--- src/App.js
+++ src/App.js
@@ -24,147 +24,88 @@ const ColumnContainer = styled.div`
   gap: 1rem;
 `
 
-const ActionButton = styled.button`
-  background-color: rgba(98, 121, 174, 0.46);
-  color: #fff;
-  padding: 0.6rem 1.2rem;
-  border-radius: 10px;
-  cursor: pointer;
-`
-
-const CONTRACT = '0x4E974c3BC95F9e5637EF77BE165BE09c46C2e12f'
+const CONTRACT_ADDRESS = '0x4E974c3BC95F9e5637EF77BE165BE09c46C2e12f'
 
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date())
   const [timezone, setTimezone] = useState('')
   const [location, setLocation] = useState('taipei')
-  const [currentAccount, setCurrentAccount] = useState(null)
-
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { ethereum } = window
-
-      if (!ethereum) {
-        alert('Please connect to MetaMask first')
-        return
-      }
-
-      const accounts = await ethereum.request({ method: 'eth_accounts' })
-      console.log('Connected wallet: ', accounts[0])
-
-      if (accounts.length !== 0) {
-        const account = accounts[0]
-        setCurrentAccount(account)
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const handleMint = async () => {
-    console.log('Click mint')
-
-    try {
-      const { ethereum } = window
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const connectedContract = new ethers.Contract(
-          CONTRACT,
-          CreativeNFTPOC.abi,
-          signer
-        )
-
-        console.log('connectedContract: ', connectedContract)
-
-        let nftTxn = await connectedContract.mint(1)
-
-        await nftTxn
-        console.log('nftTxn: ', nftTxn.hash)
-      } else {
-        alert('Please connect to wallet first and try again.')
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const [isLoading, setIsLoading] = useState(false)
 
   const getSeed = async (tokenId) => {
     try {
-      const { ethereum } = window
-
-      if (!ethereum) {
-        alert('Please connect to MetaMask first')
-        return
-      }
-
-      const accounts = await ethereum.request({ method: 'eth_accounts' })
       const infuraProvider = new ethers.providers.JsonRpcProvider(
         'https://rinkeby.infura.io/v3/7d943fe8904d4be9b833a086c1f4a566'
       )
-      const signer = infuraProvider.getSigner(accounts[0])
       const connectedContract = new ethers.Contract(
-        CONTRACT,
+        CONTRACT_ADDRESS,
         CreativeNFTPOC.abi,
-        signer
+        infuraProvider
       )
 
       console.log('connectedContract: ', connectedContract)
 
       const seed = await connectedContract.getSeed(tokenId)
-
       return seed
     } catch (error) {
-      console.log(error)
+      console.log('Error: reading seed from contract', error)
     }
   }
 
   const getLocalTime = async (location) => {
-    const res = await fetch(
-      `https://timezone.abstractapi.com/v1/current_time/?api_key=fe70b53cf8d54ed4ac6b92eff7547006&location=${location}`
-    )
-    const data = await res.json()
+    try {
+      const res = await fetch(
+        `https://timezone.abstractapi.com/v1/current_time/?api_key=fe70b53cf8d54ed4ac6b92eff7547006&location=${location}`
+      )
+      const data = await res.json()
 
-    setTimezone(data.timezone_location)
-    setLocation(data.timezone_location)
+      setTimezone(data.timezone_location)
+      setLocation(data.timezone_location)
 
-    console.log('Location Data: ', data)
+      console.log('Location Data: ', data)
+    } catch (error) {
+      console.log('Error: fetching data from abstractapi', error)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   const getLocation = async (tokenId) => {
-    const seed = await getSeed(tokenId)
+    try {
+      setIsLoading(true)
+      const seed = await getSeed(tokenId)
 
-    if (seed) {
-      console.log('seed: ', seed)
-      if (
-        Number(seed) ===
-        Number(
-          0x0000000000000000000000000000000000000000000000000000000000000000
-        )
-      ) {
-        alert('Token id does not exist. Please try another token id.')
-        return
+      if (seed) {
+        console.log('seed: ', seed)
+        if (
+          Number(seed) ===
+          Number(
+            0x0000000000000000000000000000000000000000000000000000000000000000
+          )
+        ) {
+          alert('Token id does not exist. Please try another token id.')
+          return
+        }
+
+        const parsed = parseInt(Number(seed), 10)
+        console.log('parsed number from seed: ', parsed)
+
+        const matchedLocation = locations.find(
+          (location) => location.id === parsed
+        ).location
+
+        console.log(matchedLocation)
+        getLocalTime(matchedLocation)
       }
-
-      const parsed = parseInt(Number(seed), 10)
-      console.log('parsed number from seed: ', parsed)
-
-      const matchedLocation = locations.find(
-        (location) => location.id === parsed
-      ).location
-
-      console.log(matchedLocation)
-      getLocalTime(matchedLocation)
+    } catch (error) {
+      console.log(error)
     }
   }
 
   // Check tokenId query and call getSeed(id)
   // Parse seed and match location
   // Send location request and show time
-
   useEffect(() => {
-    checkIfWalletIsConnected()
-
     const tokenIdQuery = new URLSearchParams(window.location.search).get(
       'tokenid'
     )
@@ -195,7 +136,11 @@ function App() {
           </p>
         </ColumnContainer>
       ) : (
-        <ActionButton onClick={handleMint}>Mint</ActionButton>
+        <p>
+          {isLoading
+            ? 'Getting city info...'
+            : 'Seach Token ID on URL to get city info'}
+        </p>
       )}
     </Container>
   )
